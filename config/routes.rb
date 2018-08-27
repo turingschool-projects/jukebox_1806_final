f289a0b3568afd78e198c2d7f010f85e4fc91554
ian douglas <ian.douglas@iandouglas.com>
2018-08-27 15:34:28 -0600

    nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   root 'songs#index'
   resources :songs, only: [:index]
-  resources :artists, only: [:new, :create, :index]
+
+  resources :artists, only: [:new, :create, :index], shallow: true do
+    resources :songs, only: [:show, :new, :create]
+  end
+
   resources :playlists, only: [:new, :create, :show]
+
 end
