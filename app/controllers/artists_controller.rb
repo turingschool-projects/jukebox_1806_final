65acfcc49094ec448f8a619fad9846a52065d70d
Dione Wilson <dionew1@gmail.com>
2018-08-27 10:25:22 -0600

    Forms Lesson Complete

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,24 @@
+class ArtistsController < ApplicationController
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    artist = Artist.new(artist_params)
+    if artist.save
+      redirect_to "/artists"
+    else
+      render :new
+    end
+  end
+
+  def index
+    @artists = Artist.all
+  end
+
+  private
+    def artist_params
+      params.require(:artist).permit(:name)
+    end
+end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 describe Song, type: :model do
   describe 'relationships' do
     it {should belong_to(:artist)}
+    it {should have_many(:playlist_songs)}
+    it {should have_many(:playlists).through(:playlist_songs)}
   end
 end
