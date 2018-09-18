092a8e4b60c4236b3b0d694c2ad8a46e12b67901
Patrick Shobe <patrick@patrickshobe.com>
2018-09-18 10:38:54 -0600

    adds rating validation

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -6,4 +6,7 @@ describe Song, type: :model do
     it {should have_many(:playlist_songs)}
     it {should have_many(:playlists).through(:playlist_songs)}
   end
+  describe 'validation' do
+    it {should validate_inclusion_of(:rating).in?([1,2,3,4,5])}
+  end
 end
