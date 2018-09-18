28fc930420e7e670539d4657be4387d864775f3a
Harper Bellows <harper.bellows@gmail.com>
2018-09-18 15:49:06 -0600

    add user story 7

--- spec/features/user_sees_a_song_spec.rb
+++ spec/features/user_sees_a_song_spec.rb
@@ -16,5 +16,21 @@ describe 'Song Show Page' do
       expect(page).to have_content("Play Count: #{song_1.play_count}") 
       expect(page).to have_content("Rating: #{song_1.rating}") 
     end
+    it 'shows all genres assigned to the song' do
+      artist_1 = Artist.create(name: "Rick James")
+      artist_2 = Artist.create(name: "Vanilla Ice")
+      song_1 = artist_1.songs.create(title: 'Superfreak', length: 300, play_count: 5000000, rating: 5)
+      song_2 = artist_2.songs.create(title: 'Ice Ice Baby', length: 200, play_count: 10000, rating: 1)
+      genre1 = song_1.genres.create(name: "Trip Hop")
+      genre2 = song_1.genres.create(name: "Weirdness")
+      genre3 = song_2.genres.create(name: "Classical")
+
+      visit song_path(song_1.slug)
+
+      expect(page).to have_content(genre1.name)
+      expect(page).to have_content(genre2.name)
+
+      expect(page).to_not have_content(genre3.name)
+    end
   end
 end
