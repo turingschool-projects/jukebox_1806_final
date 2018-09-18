94854fed19e65d541ee5265e6c9a743517572097
bghalami <benjaminghalami@gmail.com>
2018-09-18 10:39:29 -0600

    fourth commit

--- spec/features/visitor_visits_song_show_page_spec.rb
+++ spec/features/visitor_visits_song_show_page_spec.rb
@@ -24,5 +24,33 @@ describe "As an unregistered user" do
       visit song_path(song_2.slug)
       expect(page).to have_content("Rating: #{song_2.rating}")
     end
+    it "should show the genres for song on show page" do
+      artist_1 = Artist.create(name: "Rick James")
+      artist_2 = Artist.create(name: "Vanilla Ice")
+
+      song_1 = artist_1.songs.create!( title: 'Superfreak',
+                                      length: 300,
+                                      play_count: 5000000,
+                                      artist: artist_1
+                                    )
+      song_3 = artist_2.songs.create!( title: 'Ice Ice Baby',
+                                      length: 200,
+                                      play_count: 10000,
+                                      artist: artist_2,
+                                      rating: 3
+                                    )
+      genre_1 = Genre.create(name: "Freaks")
+      genre_2 = Genre.create(name: "Geeks")
+      genre_3 = Genre.create(name: "Rock")
+
+      song_1.genres << genre_1
+      song_1.genres << genre_2
+
+      visit song_path(song_1.slug)
+      expect(page).to have_content("Genres for this Song:")
+      expect(page).to have_link(genre_1.name)
+      expect(page).to have_link(genre_2.name)
+      expect(page).to_not have_link(genre_3.name)
+    end
   end
 end
