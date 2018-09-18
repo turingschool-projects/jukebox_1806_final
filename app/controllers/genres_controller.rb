7cfbf79f8ffc9944dcc8e602fa4b592316aa4487
Nick Lindeberg <lindebergnick@gmail.com>
2018-09-18 10:13:07 -0600

    got currentadmin to work thanks ian for the tip

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,9 +1,19 @@
 class GenresController < ApplicationController
 
+  def create
+    genre = Genre.create(genre_params)
+    redirect_to genres_path
+  end
 
   def index
     @genres = Genre.all
     @genre = Genre.new
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 
 end
