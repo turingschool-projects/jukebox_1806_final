db8f3e42e43ed2be3e8c4da87863fd67a97aa313
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-18 09:39:39 -0600

    migration adds Genre

--- spec/features/user_sees_one_song_spec.rb
+++ spec/features/user_sees_one_song_spec.rb
@@ -6,7 +6,6 @@ describe 'user visits song show page' do
     song_1 = artist_1.songs.create(title: 'Superfreak', length: 300, play_count: 5000000, rating: 5)
 
     visit song_path(song_1.slug)
-    save_and_open_page
 
     expect(page).to have_content("Rating for #{song_1.title}: #{song_1.rating}")
 
