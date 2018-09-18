2af5b609197913d763c451449324f60979ac83fd
Kathleen Yruegas <kathleenecn@gmail.com>
2018-09-18 09:41:21 -0600

    Adds test to create new genre

--- spec/features/admin_sees_all_genres_spec.rb
+++ spec/features/admin_sees_all_genres_spec.rb
@@ -20,5 +20,23 @@ describe "As an admin" do
       expect(current_path).to eq(new_admin_genre_path)
       expect(page).to have_content("Make New Genre!")
     end
+
+    it 'should be able to make a new genre' do
+      admin = User.create!(username: "dk", password: "test", role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+      genre_1 = Genre.create!(name: "Blues")
+      genre_2 = Genre.create!(name: "Alternative")
+      genre_3 = Genre.create!(name: "Trip Hop")
+
+      visit new_admin_genre_path
+
+      new_name = "Country"
+
+      fill_in :genre_name, with: new_name
+      click_on("Create Genre")
+
+      expect(current_path).to eq(genres_path)
+      expect(page).to have_content(new_name)
+    end
   end
 end
