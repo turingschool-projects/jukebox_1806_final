ed6aa5f0b3f9e82c14f0987c607cffbc31e3eba9
Mike McKee <mikecm@gmail.com>
2018-09-18 10:15:57 -0600

    user story 7

--- spec/features/visitor_song_show_spec.rb
+++ spec/features/visitor_song_show_spec.rb
@@ -2,13 +2,26 @@ require 'rails_helper'
 
 describe 'As a visitor' do
 	describe 'when I visit a song show page' do
+		before(:each) do
+			@artist = Artist.create(name: 'STS9')
+			@song   = @artist.songs.create(title: 'Moonsocket', length: 300, play_count: 20, rating: 5)
+			@genre1 = @song.genres.create(name: 'Livetronica')
+			@genre2 = @song.genres.create(name: 'Instrumental')
+			@genre3 = Genre.create(name: 'Americana')
+			@genre4 = Genre.create(name: 'Bluegrass')
+		end
 		it 'shows the numeric rating for a song' do
-			artist = Artist.create(name: 'STS9')
-			song   = artist.songs.create(title: 'Moonsocket', length: 300, play_count: 20, rating: 5)
+			visit song_path(@song.slug)
 
-			visit song_path(song.slug)
+			expect(page).to have_content("Rating: #{@song.rating}")
+		end
+		it 'displays genres associated with this song' do
+			visit song_path(@song.slug)
 
-			expect(page).to have_content("Rating: #{song.rating}")
+			expect(page).to have_content(@genre1.name)
+			expect(page).to have_content(@genre2.name)
+			expect(page).to_not have_content(@genre3.name)
+			expect(page).to_not have_content(@genre4.name)
 		end
 	end
 end
