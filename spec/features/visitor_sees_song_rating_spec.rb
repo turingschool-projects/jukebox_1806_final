54a0373543204600371b5892126cf5ba67f8345b
Kathleen Yruegas <kathleenecn@gmail.com>
2018-09-18 09:20:38 -0600

    Allows user to view rating

--- spec/features/visitor_sees_song_rating_spec.rb
+++ spec/features/visitor_sees_song_rating_spec.rb
@@ -3,9 +3,10 @@ require "rails_helper"
 describe "As a visitor" do
   describe "when I visit a song show page" do
     it 'should show song rating for the song' do
-      song = Song.create(title: "Song Title", length: 321, play_count: 12244, rating: 4)
+      artist = Artist.create!(name: "Person")
+      song = artist.songs.create!(title: "Song Title", length: 321, play_count: 12244, rating: 4)
 
-      visit song_path(song)
+      visit song_path(song.slug)
 
       expect(page).to have_content(song.title)
       expect(page).to have_content("Song Rating: #{song.rating}")
