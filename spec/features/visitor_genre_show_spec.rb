5597717d4d7d8e962c7314a030057f52ab902cfe
dmeskis <dmeskis@gmail.com>
2018-09-18 09:48:24 -0600

    User story 6 has requirements completed

--- spec/features/visitor_genre_show_spec.rb
+++ spec/features/visitor_genre_show_spec.rb
@@ -14,12 +14,18 @@ describe 'as a visitor' do
                           length: 100,
                           play_count: 50,
                           rating: 5)
-
+      genre_2 = Genre.create(name: "Funk")
+      genre_2.songs.create!(title: "Mount Crushmore",
+                          artist: artist,
+                          length: 100,
+                          play_count: 50,
+                          rating: 5)
       visit genre_path(genre)
 
       expect(page).to have_content(genre.name)
       expect(page).to have_content(genre.songs.first.title)
       expect(page).to have_content(genre.songs.last.title)
+      expect(page).to_not have_content(genre_2.songs.first.title)
     end
   end
 end
