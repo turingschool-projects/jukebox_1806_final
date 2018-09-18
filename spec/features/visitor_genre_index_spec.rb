3382e6f08acbc738839932d4bdfaa07baec62393
Mike McKee <mikecm@gmail.com>
2018-09-18 09:51:47 -0600

    user story 5

--- spec/features/visitor_genre_index_spec.rb
+++ spec/features/visitor_genre_index_spec.rb
@@ -2,14 +2,27 @@ require 'rails_helper'
 
 describe 'As a visitor' do
 	describe 'When I visist genre index page' do
+		before(:each) do
+			@genre1 = Genre.create(name: 'Alternative Rock')
+			@genre2 = Genre.create(name: 'Americana')
+		end
 		it 'displays all genres in the database' do
-			genre1 = Genre.create(name: 'Alternative Rock')
-			genre2 = Genre.create(name: 'Americana')
-
 			visit genres_path
 
-			expect(page).to have_content(genre1.name)
-			expect(page).to have_content(genre2.name)
+			expect(page).to have_content(@genre1.name)
+			expect(page).to have_content(@genre2.name)
+		end
+		it 'does not display form to create genre' do
+			visit genres_path
+
+			expect(page).to_not have_field(:genre_name)
+			expect(page).to_not have_button('Create Genre')
+		end
+		it 'has links to genre show page' do
+			visit genres_path
+
+			expect(page).to have_link(@genre1.name, href: genre_path(@genre1))
+			expect(page).to have_link(@genre2.name, href: genre_path(@genre2))
 		end
 	end
 end
