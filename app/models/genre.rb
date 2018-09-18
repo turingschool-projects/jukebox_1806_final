562daef5fbd8431b9ce24def02ec7fa0a030fe9f
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-09-18 10:42:35 -0600

    adding more test coverage

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,8 +1,9 @@
 class Genre < ApplicationRecord
+  validates_presence_of :name
   has_many :genre_songs
   has_many :songs, through: :genre_songs
 
   def average_rating
-    songs.average(:rating)
+    songs.average(:rating).to_f.round(2)
   end
 end
