b61d87376ae400ca58cd4aa42f8c79e4ec7f8eb2
dmeskis <dmeskis@gmail.com>
2018-09-18 09:41:14 -0600

    made tests for user story 4 more comprehensive

--- spec/features/admin_sees_new_genre_form_on_genre_index_spec.rb
+++ spec/features/admin_sees_new_genre_form_on_genre_index_spec.rb
@@ -29,12 +29,25 @@ describe "User visits genres index" do
 
   context "as a default user" do
     it "does not allow default user to see new genre form" do
+      artist = Artist.create(name: "Bob")
+      song = artist.songs.create!(title: "A",
+                                  length: 100,
+                                  play_count: 50,
+                                  rating: 5)
+      song_2 = artist.songs.create!(title: "B",
+                                  length: 100,
+                                  play_count: 50,
+                                  rating: 5)
+      song.genres.create(name: "Dance")
+      song_2.genres.create(name: "Theater")
       user = User.create(username: "Dione", password: "test123")
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      visit admin_categories_path
+      visit genres_path
 
+      expect(page).to have_content(song.genres.first.name)
+      expect(page).to have_content(song_2.genres.first.name)
       expect(page).to_not have_css(".new_genre")
     end
   end
