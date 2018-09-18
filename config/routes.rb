502b4e3245b1c90a886432a5ba4a8177b8398808
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-18 10:06:47 -0600

    ATP for story 6

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
 
   resources :carts, only: [:create]
 
-  resources :genres, only: [:index]
+  resources :genres, only: [:index, :show]
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
---
+++ spec/features/user_sees_one_genre_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'user visits genre show' do
+  it 'displays associated songs' do
+    artist_1 = Artist.create(name: "Rick James")
+    artist_2 = Artist.create(name: "Vanilla Ice")
+    song_1 = artist_1.songs.create(title: 'Superfreak', length: 300, play_count: 5000000)
+    song_2 = artist_2.songs.create(title: 'Ice Ice Baby', length: 200, play_count: 10000)
+    song_3 = artist_2.songs.create(title: 'Another Song', length: 400, play_count: 100)
+    genre1 = Genre.create!(name: "Pop")
+    genre2 = Genre.create!(name: "Rap")
+    song_genre1 = SongGenre.create!(song_id: song_1.id, genre_id: genre1.id)
+    song_genre2 = SongGenre.create!(song_id: song_2.id, genre_id: genre1.id)
+    song_genre3 = SongGenre.create!(song_id: song_3.id, genre_id: genre2.id)
+
+    visit genre_path(genre1)
+
+    expect(page).to have_content(song_1.title)
+    expect(page).to have_content(song_2.title)
+    expect(page).to_not have_content(song_3.title)
+  end
+end
