ace4da789f575f5a081e051ef6aacb777005ab31
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-09-18 10:55:19 -0600

    Story 8

--- spec/features/visitor_sees_a_genre_show_page_spec.rb
+++ spec/features/visitor_sees_a_genre_show_page_spec.rb
@@ -20,9 +20,10 @@ describe "As a visitor" do
     it "shows an average rating for all songs" do
       genre_1 = Genre.create!(name: "Funk")
       artist_1 = Artist.create(name: "Rick James")
+      artist_2 = Artist.create(name: "Michael Jackson")
       song_1 = genre_1.songs.create(artist_id: artist_1.id, title: 'Superfreak', length: 300, play_count: 5000000, rating: 1)
       song_2 = genre_1.songs.create(artist_id: artist_1.id, title: 'Mary Jane', length: 300, play_count: 5000000, rating: 3)
-
+      song_3 = artist_2.songs.create(title: "Billie Jean", length: 3000, play_count: 5000000, rating: 3)
       visit genre_path(genre_1)
 
       expect(page).to have_content("Average Rating: 2")
--- spec/features/visitor_sees_a_song_show_page_spec.rb
+++ spec/features/visitor_sees_a_song_show_page_spec.rb
@@ -5,16 +5,18 @@ describe "As a visitor" do
     it "displays all genres associated with that song" do
       genre_1 = Genre.create!(name: "Funk")
       artist_1 = Artist.create(name: "Rick James")
-      song_1 = genre_1.songs.create(artist_id: artist_1.id, title: 'Superfreak', length: 300, play_count: 5000000, rating: 1)
+      song_1 = genre_1.songs.create!(artist_id: artist_1.id, title: 'Superfreak', length: 300, play_count: 5000000, rating: 1)
       song_1.genres.create(name: "Soul")
       song_1.genres.create(name: "Hits")
-      
-      visit song_path(song_1)
+      Genre.create(name: "SomeOtherGenre")
+
+      visit song_path(song_1.slug)
 
       expect(page).to have_content("Genres For This Song")
       expect(page).to have_content("Soul")
       expect(page).to have_content("Hits")
       expect(page).to have_content("Funk")
+      expect(page).to_not have_content("SomeOtherGenre")
     end
   end
 end
