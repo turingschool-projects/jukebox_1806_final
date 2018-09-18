941c6e743b9541147864c9175700c9e96fd7a914
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-18 10:35:36 -0600

    add tests that default users and visitors cannot see create genre form

--- spec/features/admin_views_genre_index_page_spec.rb
+++ spec/features/admin_views_genre_index_page_spec.rb
@@ -16,8 +16,8 @@ describe 'genre index' do
       expect(page).to have_content(genre_2.name)
     end
     scenario 'admin can also see a form to create a new genre' do
-      # admin = User.create!(name: "Dr.Who", email: "thedoctor@tardis.com", password: "blue", password_confirmation: "blue", role: 1)
-      # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+      admin = User.create!(username: "Dr.Who", password: "blue", password_confirmation: "blue", role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
 
       visit genres_path
@@ -25,6 +25,25 @@ describe 'genre index' do
       expect(page).to have_content("Create new genre:")
       expect(page).to have_content("Name:")
       expect(page).to have_button("Create Genre")
+    end
+  end
+  context 'as default user' do
+    scenario 'I can not see admin create genre form' do
+      user = User.create!(username: "Rory", password: "Amelia", password_confirmation: "Amelia", role: 0)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit genres_path
+
+      expect(page).to_not have_content("Create new genre:")
+      expect(page).to_not have_button("Create Genre")
+    end
+  end
+  context 'as visitor' do
+    scenario 'I can not see admin create genre show form' do
+      visit genres_path
+
+      expect(page).to_not have_content("Create new genre:")
+      expect(page).to_not have_button("Create Genre")
     end
   end
 end
