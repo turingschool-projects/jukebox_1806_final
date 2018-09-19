b91a761fb57b31f0f27d8ac39e445843bbe9a173
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-19 09:30:06 -0600

    add awards index feature test, route, controller and index action

---
+++ app/controllers/awards_controller.rb
@@ -0,0 +1,4 @@
+class AwardsController < ApplicationController
+  def index
+  end 
+end
--- spec/features/user_views_awards_index_spec.rb
+++ spec/features/user_views_awards_index_spec.rb
@@ -8,3 +8,16 @@
 # Testing requirements:
 # - users should see at least 2 awards listed on the page
 # Admin User
+
+require 'rails_helper'
+
+describe 'award index' do
+  it 'user can see all awards' do
+    award_1 = Award.create!(name: "golden globe", year: 2002)
+    award_2 = Award.create!(name: "golden globe", year: 2002)
+    visit awards_path
+
+    expect(page).to have_link(award_1.name)
+    expect(page).to have_link(award_2.name)
+  end
+end
