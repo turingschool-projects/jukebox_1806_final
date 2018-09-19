ebf8a40264400e979db7475a9cccd9e70bb95d24
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-19 09:43:09 -0600

    add feature test for user story 2

--- spec/features/user_views_awards_index_spec.rb
+++ spec/features/user_views_awards_index_spec.rb
@@ -9,15 +9,60 @@
 # - users should see at least 2 awards listed on the page
 # Admin User
 
+# ## User Story #2
+#
+# As an admin user,
+#   When I visit the same awards index page that visitors see,
+#     I can see a form to create a new award
+#
+# Testing requirements:
+# - users should see at least 2 awards listed on the page, plus the form
+
 require 'rails_helper'
 
 describe 'award index' do
-  it 'user can see all awards' do
-    award_1 = Award.create!(name: "golden globe", year: 2002)
-    award_2 = Award.create!(name: "golden globe", year: 2002)
-    visit awards_path
+  award_1 = Award.create!(name: "golden globe", year: 2002)
+  award_2 = Award.create!(name: "golden globe", year: 2002)
 
-    expect(page).to have_link(award_1.name)
-    expect(page).to have_link(award_2.name)
+  before do
+    visit awards_path
+  end
+
+  describe 'as a visitor' do
+    it 'visitor can see all awards' do
+
+      expect(page).to have_link(award_1.name)
+      expect(page).to have_link(award_2.name)
+      expect(page).to_not have_content("Create a new award:")
+      expect(page).to_not have_content("Name")
+      expect(page).to_not have_content("Year")
+      expect(page).to_not have_button("Create Award")
+    end
+  end
+  describe 'as a user' do
+    it 'user can see all awards' do
+      user = User.create(username: "Elfo", password: "enchanted", role: 0)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      expect(page).to have_link(award_1.name)
+      expect(page).to have_link(award_2.name)
+      expect(page).to_not have_content("Create a new award:")
+      expect(page).to_not have_content("Name")
+      expect(page).to_not have_content("Year")
+      expect(page).to_not have_button("Create Award")
+    end
+  end
+  describe 'as an admin' do
+    it 'admin can see all awards and a create award form' do
+      admin = User.create(username: "Dumbledore", password: "hogwarts", role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      expect(page).to have_link(award_1.name)
+      expect(page).to have_link(award_2.name)
+      expect(page).to have_content("Create a new award:")
+      expect(page).to have_content("Name")
+      expect(page).to have_content("Year")
+      expect(page).to have_button("Create Award")
+    end
   end
 end
