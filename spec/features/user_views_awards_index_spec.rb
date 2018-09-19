b3d4eb0f300f1c57379972e219050084ba4b2ec4
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-19 09:09:25 -0600

    initial commit

---
+++ spec/features/user_views_awards_index_spec.rb
@@ -0,0 +1,10 @@
+# ## User Story #1
+#
+# As a Visitor,
+#   When I visit the awards index,
+#     I see all awards
+#       and each award should be a link to that award's show page.
+#
+# Testing requirements:
+# - users should see at least 2 awards listed on the page
+# Admin User
