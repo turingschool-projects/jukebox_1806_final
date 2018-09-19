ee94bc8b8bb631f7f5a599fb0534b16cedbe6c67
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-19 10:29:18 -0600

    Wip user story 5

--- spec/features/user_sees_all_songs_spec.rb
+++ spec/features/user_sees_all_songs_spec.rb
@@ -12,11 +12,15 @@
 require 'rails_helper'
 
 describe 'song index' do
-  it 'user can see all songs' do
+  it 'user can see all songs and awards for each song' do
     artist_1 = Artist.create(name: "Rick James")
     artist_2 = Artist.create(name: "Vanilla Ice")
-    song_1 = artist_1.songs.create(title: 'Superfreak', length: 300, play_count: 5000000)
-    song_2 = artist_2.songs.create(title: 'Ice Ice Baby', length: 200, play_count: 10000)
+    song_1 = Song.create(title: 'Superfreak', length: 300, play_count: 5000000, artist_id: artist_1.id)
+    song_2 = Song.create(title: 'Ice Ice Baby', length: 200, play_count: 10000, artist_id: artist_2.id)
+    award_1 = song_1.awards.create(name: "the best", year: 2013)
+    award_2 = song_1.awards.create(name: "the best", year: 2013)
+    award_3 = song_2.awards.create(name: "the best", year: 2013)
+    award_4 = song_2.awards.create(name: "the best", year: 2013)
 
     visit songs_path
 
@@ -24,5 +28,9 @@ describe 'song index' do
     expect(page).to have_content("Play Count: #{song_1.play_count}")
     expect(page).to have_content(song_2.title)
     expect(page).to have_content("Play Count: #{song_2.play_count}")
+    expect(page).to have_content(award_1.name)
+    expect(page).to have_content(award_2.name)
+    expect(page).to have_content(award_3.name)
+    expect(page).to have_content(award_4.name)
   end
 end
