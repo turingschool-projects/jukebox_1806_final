7180005a862794993be58d48eb8497be60aee29b
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-19 10:58:39 -0600

    progress with story 7

--- spec/features/award_show_page_spec.rb
+++ spec/features/award_show_page_spec.rb
@@ -15,17 +15,15 @@
 require 'rails_helper'
 
 describe 'award show' do
-
-  artist_1 = Artist.create(name: "Rick James")
-  artist_2 = Artist.create(name: "Vanilla Ice")
-  award_1 = Award.create!(name: "golden globe", year: 2002)
-  award_2 = Award.create!(name: "something else", year: 2002)
-  song_1 = award_1.songs.create(title: 'Superfreak', length: 300, play_count: 5000000, artist_id: artist_1.id)
-  song_2 = award_1.songs.create(title: 'Shazam', length: 300, play_count: 5000000, artist_id: artist_1.id)
-  song_3 = award_2.songs.create(title: 'Ice Ice Baby', length: 200, play_count: 10000, artist_id: artist_2.id)
-
   describe 'as a visitor' do
     it 'visitor can see all show page for award' do
+      artist_1 = Artist.create(name: "Rick James")
+      artist_2 = Artist.create(name: "Vanilla Ice")
+      award_1 = Award.create!(name: "golden globe", year: 2002)
+      award_2 = Award.create!(name: "something else", year: 2002)
+      song_1 = award_1.songs.create(title: 'Superfreak', length: 300, play_count: 5000000, artist_id: artist_1.id)
+      song_2 = award_1.songs.create(title: 'Shazam', length: 300, play_count: 5000000, artist_id: artist_1.id)
+      song_3 = award_2.songs.create(title: 'Ice Ice Baby', length: 200, play_count: 10000, artist_id: artist_2.id)
       visit award_path(award_1)
 
       expect(page).to have_content(award_1.name)
