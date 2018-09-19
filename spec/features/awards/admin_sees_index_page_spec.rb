c07000a0f311045e68fd95c611027d9224965d2d
andrewetobin <andrewetobin@gmail.com>
2018-09-19 09:37:18 -0600

    admin creates an award

--- spec/features/awards/admin_sees_index_page_spec.rb
+++ spec/features/awards/admin_sees_index_page_spec.rb
@@ -15,6 +15,25 @@ describe "admin visit index page" do
 		expect(page).to have_button('Create Award')
     expect(page).to have_content("Create a New Award")
   end
+  it "admin can add a award" do
+    admin = User.create(username: "Admin", password: "1234", role: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    award_1 = Award.create(name: "Chicago", year: 1993)
+    award_2 = Award.create(name: "Denver", year: 2003)
+
+    visit awards_path
+
+    fill_in :award_name, with: "Nobel"
+    fill_in :award_year, with: 1999
+
+    click_on "Create Award"
+
+    expect(current_path).to eq(awards_path)
+    expect(page).to have_content("Nobel")
+    expect(page).to have_content(award_1.name)
+    expect(page).to have_content(award_2.name)
+  end
 
 end
 
