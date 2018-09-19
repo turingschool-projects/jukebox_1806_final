b751945045563fe62011922f74716de1c09d56fe
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-19 09:24:45 -0600

    make model tests pass

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,8 @@ class Song < ApplicationRecord
   belongs_to :artist
   has_many :playlist_songs
   has_many :playlists, through: :playlist_songs
-
+  has_many :song_awards
+  has_many :awards, through: :song_awards
   before_save :generate_slug
 
   private
